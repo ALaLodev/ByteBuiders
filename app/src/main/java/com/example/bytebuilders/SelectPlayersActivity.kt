79d06c7ef70383f7a09e17205b139629635fc7a3
ALaLodev <113071717+ALaLodev@users.noreply.github.com>
2024-10-11 01:30:16 +0200

    Subrayado de título Players

--- app/src/main/java/com/example/bytebuilders/SelectPlayersActivity.kt
+++ app/src/main/java/com/example/bytebuilders/SelectPlayersActivity.kt
@@ -1,6 +1,9 @@
 package com.example.bytebuilders
 
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.style.UnderlineSpan
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
@@ -9,6 +12,13 @@ class SelectPlayersActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.select_players)
+
+        val textView = findViewById<TextView>(R.id.titlePlayer)
+        val content = getString(R.string.Players_title)
+        val underlinedText = SpannableString(content)
+        underlinedText.setSpan(UnderlineSpan(), 0, content.length, 0)
+        textView.text = underlinedText
+
 
     }
 
